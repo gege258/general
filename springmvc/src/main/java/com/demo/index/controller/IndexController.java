46e125aba9278f2d88e38c1530523c764dfab3c4
gege258 <gegeidotech@163.com>
2017-05-08 17:06:51 +0800

    simple springmvc frame

---
+++ springmvc/src/main/java/com/demo/index/controller/IndexController.java
@@ -0,0 +1,35 @@
+package com.demo.index.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * Created by Fengyun on 5/8/2017.
+ */
+@Controller
+public class IndexController {
+    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/index")
+    public String index(ModelMap model){
+        model.addAttribute("message", "hello,Index");
+        return "index";
+    }
+
+    @RequestMapping("/test-logback")
+    @ResponseBody
+    public Date testLogback(){
+        LOG.trace("-----------------------------------trace");
+        LOG.debug("-----------------------------------debug");
+        LOG.info("-----------------------------------info");
+        LOG.warn("-----------------------------------warn");
+        LOG.error("-----------------------------------error");
+        return new Date();
+    }
+}
